f8a6a5cf74a8d68141f96d7f29ac5449b2e66a65
phuongvv <phuong.vu@jmango360.com>
2016-08-09 18:40:54 +0700

    add reactivex lib

--- src/main/java/org/vstorm83/App.java
+++ src/main/java/org/vstorm83/App.java
@@ -1,13 +1,33 @@
 package org.vstorm83;
 
+import rx.Observable;
+import rx.Observable.OnSubscribe;
+import rx.Subscriber;
+import rx.functions.Action1;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        Observable<String> test = Observable.create(new OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("test");
+            }
+        });
+        
+        test.subscribe(new Action1<String>() {
+
+            @Override
+            public void call(String t) {
+                System.out.println(t);
+            }
+        });
+        
+        System.in.read();
     }
 }
